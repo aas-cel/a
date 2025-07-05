b47994036ce9e45f3dd4c09f7e74d2cd1b793291
aas-cel <lkamoajr@gmail.com>
2025-07-05 06:07:32 +0000

    feat: production-ready StyleCPP core, real components, backend, and examples

---
+++ build/examples/basic/CMakeFiles/real_world.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/real_world.dir/real_world.cpp.o"
+  "CMakeFiles/real_world.dir/real_world.cpp.o.d"
+  "real_world"
+  "real_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/real_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/examples/basic/CMakeFiles/real_world.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for real_world.
+# This may be replaced when dependencies are built.
